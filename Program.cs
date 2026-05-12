7158e0e59e9c80a42321afeec7863bff0c2fb95e
truongtxse180558 <TruongTXSE180558@fpt.edu.vn>
2026-05-12 13:36:33 +0700

    chore: stop tracking appsettings files

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ using SWP_BE.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
-
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
